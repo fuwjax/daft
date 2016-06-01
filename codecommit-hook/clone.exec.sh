b47b8dee2a337ce256ab319a7a8e1dd289c5326d
Michael Doberenz <fuwjax@gmail.com>
2016-05-31 20:42:40 -0500

    cleaned up the clone script

--- codecommit-hook/clone.exec.sh
+++ codecommit-hook/clone.exec.sh
@@ -4,6 +4,6 @@ set -e
 git config --global credential.helper '!aws codecommit credential-helper $@'
 git config --global credential.UseHttpPath true
 
-git clone --branch $BRANCH https://git-codecommit.$REGION.amazonaws.com/v1/repos/$REPO repo
+git clone --branch "${BRANCH:-master}" https://git-codecommit.${REGION:-us-east-1}.amazonaws.com/v1/repos/$REPO repo
 cd repo
-$CMD
+${CMD:-echo success}
