37d7976253aefe4c029759e00a70e1b9b3e70033
Michael Doberenz <fuwjax@gmail.com>
2016-06-10 17:17:10 -0500

    copied the server bit from the delta project

---
+++ punk/server.js
@@ -0,0 +1,30 @@
+#! /usr/bin/env node
+const main = require('./src/main');
+const console = require('console');
+
+var serve = function(port){
+  var server = http.createServer((req, resp) => {
+    try{
+      var result = main.handler(req, resp);
+      if(result){
+        server.close(() => {
+          console.log("exiting server");
+          process.exit(result);
+        });
+      }
+    }catch(ex){
+      console.error(ex.message);
+      resp.writeHead(500);
+      resp.end(ex.message);
+    }
+  });
+  server.listen(port, () => {
+    console.log("Server connected on "+JSON.stringify(server.address()));
+  });
+}
+
+if (require.main === module) {
+  serve.apply(this, process.argv.slice(2));
+}else{
+  module.exports = exports = serve;
+}
