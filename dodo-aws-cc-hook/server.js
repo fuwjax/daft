22f52a70b113fcb6266a97c5688d3ac39d32ba17
Michael Doberenz <fuwjax@gmail.com>
2016-06-16 20:33:55 -0500

    closer, but still can't clone git

--- dodo-aws-cc-hook/server.js
+++ dodo-aws-cc-hook/server.js
@@ -3,23 +3,43 @@ const main = require('./src/main');
 const console = require('console');
 const http = require('http');
 const process = require('process');
+const toUrl = require('url').parse;
 
 var serve = function(port){
-  var server = http.createServer((req, resp) => {
-    try{
-      var result = main.handler(req, resp);
-      if(result){
-        server.close(() => {
-          console.log("exiting server");
-          process.exit(result);
+  var server = http.createServer((request, response) => {
+    var body = [];
+    request.on('data', (chunk) => {
+      body.push(chunk);
+    }).on('end', () => {
+      try{
+        main.handler({
+          method: request.method,
+          target: toUrl(request.url, true),
+          headers: request.headers,
+          body: Buffer.concat(body)
+        }, (err, resp) => {
+          if(err) {
+            console.error(err.message);
+            response.writeHead(500);
+            response.end(err.message);
+          } else {
+            response.writeHead(resp.code, resp.headers);
+            response.end(resp.body);
+          }
         });
+      } catch(ex) {
+        response.writeHead(500);
+        response.end(ex.message);
+        console.error(ex.message);
+        console.error(request.method+" "+request.url);
+        console.error(request.headers);
+        console.error(Buffer.concat(body).toString());
       }
-    }catch(ex){
-      console.error(ex.message);
-      resp.writeHead(500);
-      resp.end(ex.message);
-    }
+    });
   });
+  if(!port || port <= 0 || port >= 65535){
+    port = 80;
+  }
   server.listen(port, () => {
     console.log("Server connected on "+JSON.stringify(server.address()));
   });
--- dodo-aws-cc-hook/src/main.js
+++ dodo-aws-cc-hook/src/main.js
@@ -1,39 +1,56 @@
-const fs = require('fs');
-const yaml = require('js-yaml');
-const exec = require('child_process').exec;
-const http = require('http');
-const console = require('console');
-const AWS = require('aws-sdk');
-const sns = new AWS.SNS();
-const toUrl = require('url').parse;
-const process = require('process');
+const proc = require('child_process');
 const format = require('util').format;
+const https = require('https');
 
-module.exports.handler = function(request, response) {
-  var url = toUrl(request.url, true);
-  console.log(request.method);
-  console.log(url);
-  console.log(request.rawHeaders);
-  var body = [];
-  request.on('data', (chunk) => {
-    body.push(chunk);
-  }).on('end', () => {
-    var event = JSON.parse(Buffer.concat(body).toString());
-    var ref = event.Records[0].codecommit.references[0].ref;
-    var region = event.Records[0].awsRegion;
-    var command = event.Records[0].customData;
-    var repoArn = event.Records[0].eventSourceARN;
-    var repoName = repoArn.substring(repoArn.lastIndexOf(':') + 1);
-    var branch = ref.substring(ref.lastIndexOf('/') + 1);
-    exec(format('git clone --branch %s https://git-codecommit.%s.amazonaws.com/v1/repos/%s repo', branch, region, repo),(err, cloneout, cloneerr) =>{
-      if(err) return response.writeHead(500);
-      exec(command, {'cwd', 'repo'}, (err, stdout, stderr) => {
-        if(err) return response.writeHead(500);
-        response.writeHead(200, {
-          'Content-Type': 'text/plain; charset=UTF-8'
-        });
-        response.end(format('Clone: %s\nClone Error: %s\nCommand: %s\nCommand Error:%s', cloneout, cloneerr, stdout, stderr));        
+proc.execSync("git config --global credential.helper '!aws codecommit credential-helper $@'");
+proc.execSync("git config --global credential.UseHttpPath true");
+
+var processCommit = function(event, cb) {
+  var msg = JSON.parse(event.Message);
+  var ref = msg.Records[0].codecommit.references[0].ref;
+  var region = msg.Records[0].awsRegion;
+  var command = msg.Records[0].customData;
+  var repoArn = msg.Records[0].eventSourceARN;
+  var repo = repoArn.substring(repoArn.lastIndexOf(':') + 1);
+  var branch = ref.substring(ref.lastIndexOf('/') + 1);
+  proc.exec(format('git clone --branch %s https://git-codecommit.%s.amazonaws.com/v1/repos/%s repo', branch, region, repo),(err, cloneout, cloneerr) =>{
+    if(err) return cb(err);
+    proc.exec(command, {'cwd': 'repo'}, (err, stdout, stderr) => {
+      if(err) return cb(err);
+      cb(null, {
+        code: 200,
+        headers: {'Content-Type': 'text/plain; charset=UTF-8'},
+        body: format('Clone: \n%s\n\nClone Error: \n%s\n\nCommand: \n%s\n\nCommand Error:\n%s', cloneout, cloneerr, stdout, stderr)
       });
-    })
+    });
   });
+}
+
+var processConfirmation = function(event, cb){
+  https.get(event.SubscribeURL, (resp) => {
+    resp.on('data', (data) => {
+      cb(null, {
+        code: resp.statusCode,
+        headers: resp.headers,
+        body: data
+      })
+    });
+  }).on('error', (err) => {
+    cb(err);
+  });
+}
+
+module.exports.handler = function(req, cb) {
+  var type = req.headers['x-amz-sns-message-type'];
+  if(!type){
+    cb(null, {code: 200});
+  }
+  var event = JSON.parse(req.body.toString());
+  if(type == 'SubscriptionConfirmation'){
+    processConfirmation(event, cb);
+  }else if(type == 'Notification'){
+    processCommit(event, cb);
+  }else{
+    cb(new Error('Unexpected SNS request: '+type));
+  }
 }
